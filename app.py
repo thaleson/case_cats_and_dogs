c681d37c9126e557060afc86654ee0b577aff62b
felipe thaleson <thaleson19@hotmail.com>
2024-07-31 12:08:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
 import streamlit as st
-from PIL import Image
 import numpy as np
+from PIL import Image
 import tensorflow as tf
+from keras.preprocessing import image
+from pag.pag1 import show_results
 
-# Carregar o modelo
-model_path = "modelo/my_model.h5"
+# Carregue o modelo treinado
 try:
-    model = tf.keras.models.load_model(model_path)
+    model = tf.keras.models.load_model("modelo/my_model.h5")
 except Exception as e:
     st.error(f"Erro ao carregar o modelo: {str(e)}")
     st.stop()
 
-# Função de previsão
+# Função para fazer previsões
 def make_prediction(image_path):
     try:
         img = Image.open(image_path).convert("RGB")
@@ -20,6 +21,7 @@ def make_prediction(image_path):
         img_array = np.expand_dims(img_array, axis=0)
         prediction = model.predict(img_array)
 
+        # Interpretação da previsão
         if prediction[0, 0] > 0.5:
             result = "Cachorro"
         else:
@@ -29,19 +31,49 @@ def make_prediction(image_path):
         st.error(f"Erro ao fazer a previsão: {str(e)}")
         return None
 
-# Streamlit UI
+# Configurações do Streamlit
 st.title("Cats_and_Dogs_IA: Classificador de Gatos e Cachorros 🐶🐱🐾")
 st.subheader("Faça uma previsão: Cachorro ou Gato?")
 
 uploaded_file = st.file_uploader("Escolha uma imagem...", type=["jpg", "jpeg", "png"])
 
-if uploaded_file:
+if uploaded_file is not None:
+    # Exiba a imagem carregada
     image = Image.open(uploaded_file)
     st.image(image, caption="Imagem de Entrada", use_column_width=True)
 
-    if st.button("Fazer Previsão"):
+# Faça a previsão quando o botão for pressionado
+if st.button("Fazer Previsão"):
+    if uploaded_file is None:
+        st.warning("Por favor, carregue uma imagem antes de fazer a previsão.")
+    else:
         prediction = make_prediction(uploaded_file)
         if prediction:
             st.write(f"Isto é um : {prediction}")
         else:
             st.error("Ocorreu um erro ao tentar fazer a previsão.")
+
+# Adicione uma foto sua
+try:
+    st.sidebar.image(
+        Image.open("media/eu1.jpeg"),
+        caption="Desenvolvedor: Thaleson Silva",
+        use_column_width=True,
+    )
+except FileNotFoundError:
+    st.sidebar.warning("A imagem do desenvolvedor não foi encontrada.")
+
+linkedin_link = "[![LinkedIn](https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/thaleson-silva-9298a0296/)"
+github_link = "[![GitHub](https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white)](https://github.com/thaleson)"
+instagram_link = "[![Instagram](https://img.shields.io/badge/Instagram-E4405F?style=for-the-badge&logo=instagram&logoColor=white)](https://www.instagram.com/_thaleson/)"
+
+st.sidebar.markdown(linkedin_link, unsafe_allow_html=True)
+st.sidebar.markdown(github_link, unsafe_allow_html=True)
+st.sidebar.markdown(instagram_link, unsafe_allow_html=True)
+
+# Adicione uma seção para a análise de resultados
+st.header("Análise de Resultados")
+
+# Botão para visualizar os resultados
+if st.button("Ver Resultados"):
+    show_results()
